458816e517715134d344cd6222302efee57994ac
Snigdha Sahara <snigdhasahara@Snigdhas-MBP.fios-router.home>
2020-03-14 11:24:22 -0400

    header changes

--- src/Header/Header.js
+++ src/Header/Header.js
@@ -1,12 +1,21 @@
 import React from 'react';
 import'./Header.css';
+import logo from '../Images/images.jpeg';
 
 const Header = () => {
-    return (
-        
+    return ( 
+            <div>
+                <div>
+                    <img src={logo} alt=""/>
+                </div>
+                 
+
             <div className='header'>
+               
                 <h1>Welcome to FaceApp</h1>
             </div>
+            </div>
+           
     );
 };
 
